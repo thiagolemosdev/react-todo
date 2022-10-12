670665d821427df1bb06732e776f1ba041c8e955
root <root@vps38781.publiccloud.com.br>
2022-10-12 19:59:05 +0000

    design app concluded

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -6,18 +6,18 @@ export default function Tasks() {
   return (
     <div>
       <div className={styles.task}>
-        <input type="checkbox" name="" id="" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur exercitationem unde repellendus in cumque nihil? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, ratione?</p>
+        <input type="checkbox" name="checkbox-1" id="checkbox-1" />
+        <label htmlFor="checkbox-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur exercitationem unde repellendus in cumque nihil? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, ratione?</label>
         <Trash className={styles.trash} size={32} />
       </div>
       <div className={styles.task}>
-        <input type="checkbox" name="" id="" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur exercitationem unde repellendus in cumque nihil? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, ratione?</p>
+        <input type="checkbox" name="checkbox-2" id="checkbox-2" />
+        <label htmlFor="checkbox-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur exercitationem unde repellendus in cumque nihil? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, ratione?</label>
         <Trash className={styles.trash} size={32} />
       </div>
       <div className={styles.task}>
-        <input type="checkbox" name="" id="" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur exercitationem unde repellendus in cumque nihil? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, ratione?</p>
+        <input type="checkbox" name="checkbox-3" id="checkbox-3" />
+        <label htmlFor="checkbox-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur exercitationem unde repellendus in cumque nihil? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero, ratione?</label>
         <Trash className={styles.trash} size={32} />
       </div>
     </div>
