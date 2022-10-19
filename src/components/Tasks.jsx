4b7ca05a6c31e75c92b3c093e1f086b4cc97ef7c
Thiago Lemos <lemoos.th@gmail.com>
2022-10-18 21:45:44 -0300

    add and delete tasks functional

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Trash } from "phosphor-react";
 import styles from "./Tasks.module.css";
 
-// interface Task {
-//   id: string,
-//   note: string,
-//   isComplete: boolean,
-// }
+export default function Tasks({ id, note, isComplete, onDeleteTask, onCompleteTask }) {
 
-export default function Tasks({ id, note, isComplete }) {
+  function handleDeleteTask() {
+    onDeleteTask(id)
+  }
+
   return (
     <div>
       <div className={styles.task}>
         <input type="checkbox" name={`checkbox-${id}`} id={`checkbox-${id}`} />
         <label htmlFor={`checkbox-${id}`}>{note}</label>
-        <Trash className={styles.trash} size={32} />
+        <Trash onClick={handleDeleteTask} className={styles.trash} size={32} />
       </div>
     </div>
   );
--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -11,28 +11,14 @@ export default function TasksList() {
     event.preventDefault();
 
     input = event.target.value
-    console.log(input)
   }
-  
-  const [tasks, setTasks] = useState([
-    {
-      id: v4(),
-      note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aliquid tenetur libero unde nostrum laudantium, reprehenderit qui voluptate mollitia sapiente numquam modi ratione sunt, atque aut asperiores, aperiam aspernatur iusto!",
-      isComplete: false,
-    },
-    {
-      id: v4(),
-      note: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ratione!",
-      isComplete: false,
-    },
-  ]);
-  console.log(tasks);
 
-  const [newTask, setNewTask] = useState([{}]);
+  const [tasks, setTasks] = useState([]);
+
+  const [newTask, setNewTask] = useState([]);
 
   function handleCreateNewTasks(event) {
     event.preventDefault();
-    console.log(event.target);
 
     setTasks([
       ...tasks,
@@ -44,41 +30,78 @@ export default function TasksList() {
     ]);
   }
 
-  return (
-    <div>
-      <form onSubmit={handleCreateNewTasks} className={styles.form}>
-      <input onChange={handleNewTaskChange} type="text" placeholder='Adicione uma nova tarefa' />
-        <button onClick={handleCreateNewTasks} >Criar <PlusCircle size={16} /> </button>
-      </form>
+  function completeTask() {
 
-      <div className={styles.status}>
-        <p className={styles.created}>
-          Tarefas criadas<span>0</span>
-        </p>
-        <p className={styles.concluded}>
-          Concluidas<span>0</span>
-        </p>
+  }
+
+  function deleteTask(taskToDelete) {
+    const tasksWithoutDeletedOne = tasks.filter((task) => {
+      return task.id !== taskToDelete;
+    });
+    setTasks(tasksWithoutDeletedOne);
+  }
+
+  if (tasks.length <= 0) {
+    return (
+
+      <div>
+        <form onSubmit={handleCreateNewTasks} className={styles.form}>
+          <input onChange={handleNewTaskChange} type="text" placeholder='Adicione uma nova tarefa' />
+          <button onClick={handleCreateNewTasks} >Criar <PlusCircle size={16} /> </button>
+        </form>
+
+        <div className={styles.status}>
+          <p className={styles.created}>
+            Tarefas criadas<span>0</span>
+          </p>
+          <p className={styles.concluded}>
+            Concluidas<span>0</span>
+          </p>
+        </div>
+
+        <div className={styles.tasks}>
+          <div className={styles.empty}>
+            <ClipboardText className={styles.clipboard} size={64} />
+            <p>Você ainda não tem tarefas cadastradas <span>Crie tarefas e organize seus itens a fazer</span></p>
+          </div>
+        </div>
       </div>
+    );
 
-      <div className={styles.tasks}>
-        {tasks.map((task) => {
-          return (
-            <div>
-              <Tasks
-                key={task.id}
-                id={task.id}
-                note={task.note}
-                isComplete={task.isComplete}
-              />
-            </div>
-          );
-        })}
+  } else {
+    return (
+      <div>
+        <form onSubmit={handleCreateNewTasks} className={styles.form}>
+          <input onChange={handleNewTaskChange} type="text" placeholder='Adicione uma nova tarefa' />
+          <button onClick={handleCreateNewTasks} >Criar <PlusCircle size={16} /> </button>
+        </form>
 
-        {/* <div className={styles.empty}>
-          <ClipboardText className={styles.clipboard} size={64} />
-          <p>Você ainda não tem tarefas cadastradas <span>Crie tarefas e organize seus itens a fazer</span></p>
-        </div> */}
+        <div className={styles.status}>
+          <p className={styles.created}>
+            Tarefas criadas<span>{tasks.length}</span>
+          </p>
+          <p className={styles.concluded}>
+            Concluidas<span>0</span>
+          </p>
+        </div>
+
+        <div className={styles.tasks}>
+          {tasks.map((task) => {
+            return (
+              <div>
+                <Tasks
+                  key={task.id}
+                  id={task.id}
+                  note={task.note}
+                  isComplete={task.isComplete}
+                  onCompleteTask={completeTask}
+                  onDeleteTask={deleteTask}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
